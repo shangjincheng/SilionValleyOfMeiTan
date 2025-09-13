c285eca69c3b8621c047b011bd57b3b602cb33b9
shangjincheng <221250094@smail.nju.edu.cn>
2025-09-13 20:00:45 +0800

    处理路径重定向

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // 关键配置：所有404请求都指向index.html
+  },
 })
